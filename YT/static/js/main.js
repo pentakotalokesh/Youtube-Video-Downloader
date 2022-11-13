8d683488ca37e6b3b154668515c10e37e84cbaa7
pentakotalokesh <pentakotalokesh67@gmail.com>
2022-11-13 17:19:03 +0530

    UI-1.0

---
+++ YT/static/js/main.js
@@ -0,0 +1,14 @@
+var input = document.getElementById("l")
+var title = document.getElementById("title")
+input.addEventListener("keypress",function(event){
+    if(event.key == "Enter"){
+        event.preventDefault();
+        document.getElementById("bt").click();
+    }
+});
+function myfun(){
+    if(title.innerHTML.length!=0){
+        document.getElementById("details").style.display="flex";
+        document.getElementById("search").style.display = "none";
+    }
+}
