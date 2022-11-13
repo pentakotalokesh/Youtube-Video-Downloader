9f1260cddfb734f5b70288cd50d7b4ce17943988
pentakotalokesh <pentakotalokesh67@gmail.com>
2022-11-13 12:32:25 +0530

    Mobile

--- YTdownloader/settings.py
+++ YTdownloader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xkm$g8&@&6msjmc#ql)ej!-@wu+-!0%sd=r*5up@b^#^x$af!x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
