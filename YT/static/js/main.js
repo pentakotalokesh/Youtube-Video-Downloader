ee9d5abff4fd968a8d2e2b88c02372355057bec3
pentakotalokesh <pentakotalokesh67@gmail.com>
2022-11-14 23:08:57 +0530

    Added Loader Animation

--- YT/static/js/main.js
+++ YT/static/js/main.js
@@ -4,11 +4,15 @@ input.addEventListener("keypress",function(event){
     if(event.key == "Enter"){
         event.preventDefault();
         document.getElementById("bt").click();
+        document.getElementById("search").style.display = "none";
+        document.getElementById("ld").style.display = "grid";
+
     }
 });
 function myfun(){
     if(title.innerHTML.length!=0){
         document.getElementById("details").style.display="flex";
         document.getElementById("search").style.display = "none";
+        document.getElementById("ld").style.display = "none";
     }
 }
